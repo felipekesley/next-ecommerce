57438ed93e38493a1ae2883532df6241310fefc1
Felipe Kesley <fkesley.dev@gmail.com>
2023-12-20 16:23:33 -0300

    feat: add hero banner

--- src/components/HeroBanner/index.tsx
+++ src/components/HeroBanner/index.tsx
@@ -1,7 +1,14 @@
+import Image from 'next/image'
+
 const HeroBanner = () => {
 	return (
-		<div className="w-full h-96 bg-slate-300 flex items-center justify-center">
-			banner
+		<div className="w-full">
+			<Image
+				src="/images/banner-1.png"
+				width={1640}
+				height={650}
+				alt="Picture of the author"
+			/>
 		</div>
 	)
 }
